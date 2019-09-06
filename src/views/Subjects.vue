75a00a5fc4c6b8bd3f8fb154709076f0866f6a15
Akash Patel <akashppa@usc.edu>
2019-09-06 15:19:12 -0700

    Images for subjects

--- src/views/Subjects.vue
+++ src/views/Subjects.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col w-full">
     <h1 class="ml-2 mt-0 md:mt-4 mb-0 md:mb-4 text-lg md:text-2xl">Explore Subjects</h1>
     <div class="flex flex-wrap">
-      <Subject v-for="subject in subjectList" v-bind:key="subject" v-bind:subjectName="subject" :numCards=0 :userSubscribed="false" :comingSoon="true"></Subject>
+      <Subject v-for="subject in subjectList" v-bind:key="subject" v-bind:subjectName="subject" :numCards=0 :userSubscribed="false" :comingSoon="false"></Subject>
     </div>
   </div>
 </template>
@@ -17,7 +17,7 @@ export default {
   },
   data() {
     return {
-      subjectList: ['AP US History', 'AP World History', 'AP Biology','AP Gender Studies']
+      subjectList: ['AP US History', 'AP World History', 'AP Biology','AP US Government']
     }
   }
 }
